277556d39e90a16a7bf8343495a70c4e73defde4
Anders Klenke <anders_klenke@icloud.com>
2018-12-26 18:00:50 +0100

    Add user to the GraphQL schema

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,8 +6,16 @@ module Types
           description: 'Wine stores or wine bars.',
           null: true
 
+    field :viewer, UserType,
+          description: 'The signed in user.',
+          null: true
+
     def businesses
       Business.all
+    end
+
+    def viewer
+      context[:current_user]
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, presence: true
+
+  def short_name
+    "#{first_name} #{last_name[0]}."
+  end
 end
