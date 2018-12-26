0a2d1e8c0227e32c7ce57c0e61fed1c33fbd5847
Anders Klenke <anders_klenke@icloud.com>
2018-12-26 18:46:59 +0100

    Move navigation to React

--- app/javascript/pages/SearchPage.jsx
+++ app/javascript/pages/SearchPage.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
+import NavBar from '../components/NavBar'
 import Search from '../components/Search'
 
-const SearchPage = () => <Search />
+const SearchPage = () => (
+  <Fragment>
+    <NavBar />
+    <Search />
+  </Fragment>
+)
 
 export default SearchPage
