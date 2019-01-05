bfee301995756ed5cc06925ce64cb8aa117ac0e4
Anders Klenke <anders_klenke@icloud.com>
2019-01-05 18:30:35 +0100

    Add `search` field to the root query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,12 +10,41 @@ module Types
           description: 'The signed in user.',
           null: true
 
+    field :search, [BusinessType], null: true do
+      description(
+        'Search for a businesses with a distance of latitude and longitude.'
+      )
+
+      argument :latitude,
+               Float,
+               description: '_Latitude_ you want to find businesses near.',
+               required:    true
+
+      argument :longitude,
+               Float,
+               description: '_Longitude_ you want to find businesses near.',
+               required:    true
+
+      argument :distance,
+               Integer,
+               description: 'Distance in meters from the point.',
+               required:    false
+    end
+
     def businesses
       Business.all
     end
 
     def viewer
       context[:current_user]
+    end
+
+    def search(latitude:, longitude:, distance: 1000)
+      Business.closest_within(
+        latitude:  latitude,
+        longitude: longitude,
+        distance:  distance
+      )
     end
   end
 end
