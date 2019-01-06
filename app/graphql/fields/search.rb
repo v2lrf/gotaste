f984b8a53515108f0a2217391a51e11313c11c4c
Anders Klenke <anders_klenke@icloud.com>
2019-01-06 13:10:23 +0100

    Extract search field into module to make root query smaller

---
+++ app/graphql/fields/search.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Fields
+  module Search
+    def self.included(child_class)
+      child_class.field(:search, [Types::BusinessType], null: true) do
+        description DESCRIPTION[:search_field]
+
+        argument :latitude, Float,
+                 description: DESCRIPTION[:latitude],
+                 required:    true
+
+        argument :longitude,
+                 Float,
+                 description: DESCRIPTION[:longitude],
+                 required:    true
+
+        argument :distance,
+                 Integer,
+                 description: DESCRIPTION[:distance],
+                 required:    false
+      end
+    end
+
+    def search(latitude:, longitude:, distance: 1000)
+      Business.closest_within(
+        latitude:  latitude,
+        longitude: longitude,
+        distance:  distance
+      )
+    end
+
+    DESCRIPTION = {
+      search_field: 'Search for a businesses within a distance
+        of a given latitude and longitude.',
+      latitude: 'The WGS84 latitude of the business in decimal
+        degrees. Must be between ­-90 and +90.',
+      longitude: 'The WGS84 longitude of the business in decimal
+        degrees. Must be between ­-90 and +90',
+      distance:  'Distance in meters from the point.'
+    }.freeze
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,6 +2,8 @@
 
 module Types
   class QueryType < Types::BaseObject
+    include Fields::Search
+
     field :businesses, [BusinessType],
           description: 'Wine stores or wine bars.',
           null: true
@@ -10,41 +12,12 @@ module Types
           description: 'The signed in user.',
           null: true
 
-    field :search, [BusinessType], null: true do
-      description(
-        'Search for a businesses with a distance of latitude and longitude.'
-      )
-
-      argument :latitude,
-               Float,
-               description: '_Latitude_ you want to find businesses near.',
-               required:    true
-
-      argument :longitude,
-               Float,
-               description: '_Longitude_ you want to find businesses near.',
-               required:    true
-
-      argument :distance,
-               Integer,
-               description: 'Distance in meters from the point.',
-               required:    false
-    end
-
     def businesses
       Business.all
     end
 
     def viewer
       context[:current_user]
-    end
-
-    def search(latitude:, longitude:, distance: 1000)
-      Business.closest_within(
-        latitude:  latitude,
-        longitude: longitude,
-        distance:  distance
-      )
     end
   end
 end
