0b46955363d33fd35ec858f087e8ee5bd911c7fa
Anders Klenke <anders_klenke@icloud.com>
2019-01-06 14:39:59 +0100

    Enable Postgis in migration

--- db/migrate/20190104221518_add_longitude_latitude_to_businesses.rb
+++ db/migrate/20190104221518_add_longitude_latitude_to_businesses.rb
@@ -1,5 +1,6 @@
 class AddLongitudeLatitudeToBusinesses < ActiveRecord::Migration[5.2]
   def change
+    enable_extension 'postgis'
     add_column :businesses, :longitude_latitude, :st_point, geographic: true
     add_index :businesses, :longitude_latitude, using: :gist
   end
