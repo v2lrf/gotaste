587f6597eb18b6e188d6bb31d44b2d940bf80241
Anders Klenke <anders_klenke@icloud.com>
2019-01-06 15:03:50 +0100

    Change search to connection

--- app/graphql/fields/business_search.rb
+++ app/graphql/fields/business_search.rb
@@ -3,7 +3,7 @@
 module Fields
   module BusinessSearch
     def self.included(child_class)
-      child_class.field(:search, [Types::BusinessType], null: true) do
+      child_class.field(:search, Types::BusinessType.connection_type, null: true) do
         description DESCRIPTION[:search_field]
 
         argument :latitude, Float,
--- app/graphql/govinu_schema.rb
+++ app/graphql/govinu_schema.rb
@@ -5,4 +5,25 @@ class GovinuSchema < GraphQL::Schema
   query(Types::QueryType)
 
   use GraphQL::Batch
+
+  def self.id_from_object(object, type_definition, query_ctx)
+    # Call your application's UUID method here
+    # It should return a string
+    GraphQL::Schema::UniqueWithinType.encode(type_definition.name, object.id)
+  end
+
+  def self.object_from_id(id, query_ctx)
+    class_name, item_id = MyApp::GlobalId.decrypt(id)
+    # "Post" => Post.find(item_id)
+    Object.const_get(class_name).find(item_id)
+  end
+
+  def self.resolve_type(type, object, ctx)
+    case object
+    when Business
+      Types::BusinessType
+    else
+      raise("Unexpected object: #{object}")
+    end
+  end
 end
