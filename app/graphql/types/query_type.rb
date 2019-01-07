cd9c519d16d11def9a236f25147d7162a66e9db4
Anders Klenke <anders_klenke@icloud.com>
2019-01-07 21:50:32 +0100

    Remove `businesses` field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,10 +4,6 @@ module Types
   class QueryType < Types::BaseObject
     include Fields::BusinessSearch
 
-    field :businesses, [BusinessType],
-          description: 'Wine stores or wine bars.',
-          null: true
-
     field :business, BusinessType, "Find a business by it's ID.", null: true do
       argument :id, ID,
                description: 'ID of the business',
@@ -20,10 +16,6 @@ module Types
 
     def business(id:)
       GovinuSchema.object_from_id(id, nil)
-    end
-
-    def businesses
-      Business.all
     end
 
     def viewer
--- app/javascript/components/Search/index.jsx
+++ app/javascript/components/Search/index.jsx
@@ -1,33 +1,11 @@
 import React, { Fragment } from 'react'
-import { Query } from 'react-apollo'
-import gql from 'graphql-tag'
 
-import Loading from '../Loading'
 import SearchBar from './SearchBar'
-import SearchResults from './SearchResults'
-
-const GET_ALL_BUSINESSES = gql`
-  query GetAllBusinesses {
-    businesses {
-      name
-    }
-  }
-`
 
 const Search = () => (
-  <Query query={GET_ALL_BUSINESSES}>
-    {({ data, loading, error }) => {
-      if (loading) return <Loading />
-      if (error) return 'Error...'
-
-      return (
-        <Fragment>
-          <SearchBar />
-          <SearchResults businesses={data.businesses} />
-        </Fragment>
-      )
-    }}
-  </Query>
+  <Fragment>
+    <SearchBar />
+  </Fragment>
 )
 
 export default Search
