60740820378021cc8d0d08239b5effe465af14f4
Anders Klenke <anders_klenke@icloud.com>
2019-01-09 20:28:19 +0100

    Add open/close getter method

--- app/models/opening_hour.rb
+++ app/models/opening_hour.rb
@@ -6,4 +6,18 @@ class OpeningHour < ApplicationRecord
   def day_of_week
     Date::DAYNAMES[self[:day_of_week]]
   end
+
+  def open
+    format_time(self[:open])
+  end
+
+  def close
+    format_time(self[:close])
+  end
+
+  private
+
+  def format_time(time)
+    time&.strftime('%H:%M')
+  end
 end
--- spec/models/opening_hour_spec.rb
+++ spec/models/opening_hour_spec.rb
@@ -10,8 +10,22 @@ RSpec.describe OpeningHour, type: :model do
 
   describe '#day_of_week' do
     it 'returns the name of the day' do
-      opening_hour = FactoryBot.create(:opening_hour)
+      opening_hour = FactoryBot.build(:opening_hour)
       expect(opening_hour.day_of_week).to eq 'Monday'
+    end
+  end
+
+  describe '#open' do
+    it 'returns the time in hh:mm format' do
+      opening_hour = FactoryBot.build(:opening_hour, open: '09:00')
+      expect(opening_hour.open).to eq '09:00'
+    end
+  end
+
+  describe '#close' do
+    it 'returns the time in hh:mm format' do
+      opening_hour = FactoryBot.build(:opening_hour, close: '17:00')
+      expect(opening_hour.close).to eq '17:00'
     end
   end
 end
