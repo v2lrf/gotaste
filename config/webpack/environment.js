ac23caae61a22ac2ba809a3371294ad791129b68
Mads Obel <obel@autobutler.dk>
2019-01-11 11:18:54 +0100

    Move env file load to development only

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,3 @@
 const { environment } = require('@rails/webpacker')
-// eslint-disable-next-line import/no-extraneous-dependencies
-const webpack = require('webpack')
-const dotenv = require('dotenv')
-
-const dotenvFiles = ['.env']
-
-dotenvFiles.forEach(dotenvFile => {
-  dotenv.config({ path: dotenvFile, silent: true })
-})
-
-environment.plugins.prepend(
-  'Environment',
-  new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(process.env)))
-)
 
 module.exports = environment
