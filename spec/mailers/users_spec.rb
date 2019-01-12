3b9029c837646bb3d7b7c7d55750addc952eb115
Anders Klenke <anders_klenke@icloud.com>
2019-01-12 12:57:45 +0100

    Localize welcome email

--- spec/mailers/users_spec.rb
+++ spec/mailers/users_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe UsersMailer, type: :mailer do
     let(:mail) { described_class.welcome(user: user).deliver_now }
 
     it 'renders the headers' do
-      expect(mail.subject).to eq('Velkommen til Govinu')
+      expect(mail.subject).to eq(I18n.t('users_mailer.welcome.subject'))
       expect(mail.to).to eq([user.email])
     end
   end
