9ee97de157c00b99253801f5fe4a3cbbcd31e419
Anders Klenke <anders_klenke@icloud.com>
2019-03-06 19:56:02 +0100

    Fix rubocop offenses

--- app/graphql/types/search_type.rb
+++ app/graphql/types/search_type.rb
@@ -2,10 +2,10 @@
 
 module Types
   class SearchType < BaseUnion
-    description "Objects which may be found by search."
+    description 'Objects which may be found by search.'
     possible_types BusinessType, EventType
 
-    def self.resolve_type(object, context)
+    def self.resolve_type(object, _context)
       if object.is_a?(Business)
         Types::BusinessType
       else
