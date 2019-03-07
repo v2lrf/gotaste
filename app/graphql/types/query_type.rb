db466272d2ff9ce73ff60143c386be410690b111
Anders Klenke <anders_klenke@icloud.com>
2019-03-07 21:45:17 +0100

    Add "Find event from slug" graphql field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,6 +4,7 @@ module Types
   class QueryType < Types::BaseObject
     field :area_search, resolver: Resolvers::AreaSearch
     field :business, resolver: Resolvers::FindBusinessFromSlug
+    field :event, resolver: Resolvers::FindEventFromSlug
     field :search, resolver: Resolvers::Search
     field :viewer, resolver: Resolvers::Viewer
   end
