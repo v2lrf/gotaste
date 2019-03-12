e351b933dffbf4ea6e488c6a40a6fd38868837f3
Mads Obel <madsobelcph@gmail.com>
2019-03-12 21:39:41 +0100

    Replace emojies on alert with icons (#146)

--- client/components/Alert/index.jsx
+++ client/components/Alert/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faExclamationCircle,
+  faInfoCircle
+} from '@fortawesome/pro-light-svg-icons'
 
 function Alert({ kind, children }) {
   const alertClasses = classNames(
-    'flex border shadow rounded my-4 py-2 text-sm',
+    'flex items-center border shadow rounded my-4 py-2 text-sm',
     {
       'bg-red-lightest text-red-darkest border-red-light': kind === 'danger',
       'bg-white text-red-darkest border-red-lighter': kind === 'info'
@@ -13,8 +18,18 @@ function Alert({ kind, children }) {
   return (
     <div className={alertClasses}>
       <span className="pl-2 mr-2" role="img" aria-label="ikon" aria-hidden>
-        {kind === 'info' && '⚠️'}
-        {kind === 'danger' && '🚨'}
+        {kind === 'info' && (
+          <FontAwesomeIcon
+            icon={faInfoCircle}
+            className="text-red-light text-xl"
+          />
+        )}
+        {kind === 'danger' && (
+          <FontAwesomeIcon
+            icon={faExclamationCircle}
+            className="text-red-light text-xl"
+          />
+        )}
       </span>
       <div className="pr-2">{children}</div>
     </div>
