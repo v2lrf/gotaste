5946f0c3bbd6ae318c571ec2985525abfb44d445
Anders Klenke <anders_klenke@icloud.com>
2019-03-21 18:05:19 +0100

    Simplify areas

--- app/models/area.rb
+++ app/models/area.rb
@@ -4,9 +4,14 @@ class Area < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  acts_as_nested_set
-
   validates :name, :longitude_latitude, presence: true
+
+  belongs_to :parent, class_name: 'Area', optional: true
+  has_many :children,
+           class_name:  'Area',
+           foreign_key: 'parent_id',
+           dependent:   :restrict_with_exception,
+           inverse_of:  :parent
 
   has_many :businesses, dependent: :restrict_with_exception
 
