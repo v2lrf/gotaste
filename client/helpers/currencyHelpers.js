bb8c475e9cac8af4634076f0361c170404b14714
Mads Obel <madsobelcph@gmail.com>
2019-03-29 21:01:53 +0100

    Change currency helper to use built-ins (#210)

--- client/helpers/currencyHelpers.js
+++ client/helpers/currencyHelpers.js
@@ -1,12 +1,8 @@
 export function simpleFormat(amount) {
-  let stringAmount = amount.toString()
-
-  if (stringAmount.indexOf('.') > -1) {
-    stringAmount = stringAmount.replace('.', ',')
-  } else {
-    stringAmount = `${stringAmount},00`
-  }
-  return `${stringAmount} kr.`
+  return new Intl.NumberFormat('da-DK', {
+    style: 'currency',
+    currency: 'DKK'
+  }).format(amount)
 }
 
 export default this
