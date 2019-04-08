73348b04cc515b4243a0170134998e7421245d7e
Mads Obel <madsobelcph@gmail.com>
2019-04-08 21:45:54 +0200

    Pass on login token to ahoy (#241)

--- client/lib/ahoy.js
+++ client/lib/ahoy.js
@@ -1,3 +1,17 @@
+import Cookies from 'js-cookie'
+
 const isServerSide = typeof window === 'undefined'
-// eslint-disable-next-line global-require
-export default (isServerSide ? null : require('ahoy.js').default)
+
+// eslint-disable-next-line import/no-mutable-exports
+let ahoy = null
+
+if (!isServerSide) {
+  // eslint-disable-next-line global-require
+  ahoy = require('ahoy.js').default
+  ahoy.configure({
+    headers: { Authorization: `Bearer ${Cookies.get('token')}` },
+    withCredentials: true
+  })
+}
+
+export default ahoy
