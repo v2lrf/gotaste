a2195dd388cfc80ec467717ba0b9cd96974859b1
Anders Klenke <anders_klenke@icloud.com>
2019-04-27 19:16:29 +0200

    Change the "after sign in" path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
 
+  protected
+
+  def after_sign_in_path_for(user)
+    AfterSignInPathService.new(
+      user:               user,
+      admin_path:         admin_root_path,
+      business_user_path: private_room_root_path,
+      user_path:          root_path
+    ).call
+  end
+
   private
 
   def set_locale
