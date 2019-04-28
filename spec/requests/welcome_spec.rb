6bb4362faabce4d8ce25e22b9f44981aba37e7d8
Anders Klenke <anders_klenke@icloud.com>
2019-04-28 09:56:49 +0200

    Redirect user to correct place on welcome controller

---
+++ spec/requests/welcome_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Welcome page', type: :request do
+  before do
+    sign_in(user)
+  end
+
+  context 'when signed in as an admin' do
+    let(:user) { create(:user, :admin) }
+
+    it 'redirects to the admin panel page' do
+      get root_path
+      expect(response).to redirect_to(admin_root_path)
+    end
+  end
+
+  context 'when signed in as a employee' do
+    let(:employee) { create(:employee) }
+    let(:user)     { employee.user }
+
+    it 'redirects to the business panel page' do
+      get root_path
+      expect(response).to redirect_to(private_room_root_path)
+    end
+  end
+
+  context 'when signed in as a regular user' do
+    let(:user) { create(:user) }
+
+    it 'redirects to the main Govinu page' do
+      get root_path
+      expect(response).to redirect_to(AppConfig.main_front_page)
+    end
+  end
+end
