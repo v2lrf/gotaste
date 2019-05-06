4e0b970dba2bb449f5ba9ab045ff409365f0e08b
Anders Klenke <anders_klenke@icloud.com>
2019-05-06 21:32:02 +0200

    Add `updateBusiness` mutation

--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
   class MutationType < Types::BaseObject
     field :sign_in, mutation: Mutations::SignIn
     field :sign_up, mutation: Mutations::SignUp
+    field :update_business, mutation: Mutations::UpdateBusiness
   end
 end
