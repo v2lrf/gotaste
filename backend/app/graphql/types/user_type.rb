ee4c670d0b25fefb7add0ecbe32aca628b193b58
Anders Klenke <anders_klenke@icloud.com>
2019-05-09 09:12:00 +0200

    Add `businesses` to UserType

--- backend/app/graphql/types/user_type.rb
+++ backend/app/graphql/types/user_type.rb
@@ -9,5 +9,9 @@ module Types
     field :short_name, String,
           null:        false,
           description: '_First name_ and the initial of the _last name_.'
+
+    field :businesses, BusinessType.connection_type,
+          null:        true,
+          description: 'Businesses the user is associated with.'
   end
 end
