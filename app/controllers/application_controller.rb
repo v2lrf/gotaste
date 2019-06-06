6c30eb7248addb52a995bc00871966a997dda0b3
Anders Klenke <anders_klenke@icloud.com>
2019-06-06 20:24:08 +0200

    Remove after sign in code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,6 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
   before_action :authenticate_user!
 
-  protected
-
-  def after_sign_in_path_for(user)
-    AfterSignInPathService.new(
-      user:               user,
-      admin_path:         admin_root_path,
-      business_user_path: private_room_root_path,
-      user_path:          AppConfig.main_front_page
-    ).call
-  end
-
   private
 
   def set_locale
