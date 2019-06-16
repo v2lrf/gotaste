31b2994e4a4a2f3246c6bec0193e7dedd28fa44a
Anders Klenke <anders_klenke@icloud.com>
2019-06-16 19:13:32 +0200

    WIP 2

--- client/pages/business.jsx
+++ client/pages/business.jsx
@@ -4,12 +4,10 @@ import { gql } from 'apollo-boost'
 import { useQuery } from 'react-apollo-hooks'
 import { Image } from 'cloudinary-react'
 import ReactMarkdown from 'react-markdown'
-import {
-  faMapMarkerAlt,
-  faPhone,
-  faBrowser,
-  faDoorOpen
-} from '@fortawesome/pro-light-svg-icons'
+import { faPhone, faBrowser, faHeart } from '@fortawesome/pro-light-svg-icons'
+
+import { faHeart as faSolidHeart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import config from '../config'
 import ahoy from '../lib/ahoy'
@@ -33,6 +31,7 @@ import SimpleItem from '../components/SimpleItem'
 import EventCard from '../components/EventCard'
 import { MapWithMarker } from '../components/Map'
 import Button from '../components/Button'
+import FavouriteButton from '../components/FavouriteButton'
 
 const GET_BUSINESS = gql`
   query getBusiness($slug: String!) {
@@ -82,7 +81,6 @@ function BusinessPage({ slug }) {
   const {
     business,
     business: {
-      logoId,
       name,
       address,
       heroImageId,
@@ -111,139 +109,180 @@ function BusinessPage({ slug }) {
         address.city
       ]}
     >
-      <div className="relative">
-        <Image
-          cloudName={config.cloudinaryCloudName}
-          publicId={heroImageId}
-          height={500}
-          width={1200}
-          crop="fill"
-          className="block container mx-auto rounded-none shadow-lg"
-          secure="true"
-          alt={name}
-        />
+      <div>
+        <div className="">
+          <Image
+            cloudName={config.cloudinaryCloudName}
+            publicId={heroImageId}
+            crop="fill"
+            secure="true"
+            alt={name}
+            width="1200"
+            height="500"
+            className="shadow-md w-full object-cover mh-200"
+          />
+        </div>
 
-        <div className="absolute pin-b pin-l ml-8 mb-20 text-white">
-          <div>
-            <div className="mb-6 pb-1 text-5xl font-bold border-b-2 border-red-light tracking-wide">
-              {name}
+        <div className="relative px-4 -mt-16">
+          <div className="bg-white rounded-lg p-4 shadow-lg">
+            <div className="flex">
+              <span className="inline-block px-2 py-1 leading-none bg-red-lightest text-grey-darkest font-semibold rounded-lg">
+                {translateBusinessType(businessType)}
+              </span>
+
+              <div className="px-2 py-1 text-grey-dark font-semibold">
+                {isOpenNow && 'Åben nu'}
+              </div>
             </div>
-            <div className=" py-2 px-3 bg-black rounded font-semibold text-xs uppercase">
-              {translateBusinessType(businessType)}
+            <h1 className="my-3 font-semibold text-3xl text-red">{name}</h1>
+            <h2 className="my-3 text-xl font-normal">{shortDescription}</h2>
+            <div className="flex">
+              <span className="inline-block border rounded p-2 hover:bg-grey-lightest">
+                <FontAwesomeIcon
+                  icon={faHeart}
+                  className="text-black text-2xl"
+                />
+              </span>
             </div>
           </div>
         </div>
       </div>
-      <div className="p-8 mx-8 bg-white border rounded-lg text-2xl">Hej</div>
-      <Container>
-        <SimpleItem icon={faMapMarkerAlt}>
-          {`${address.streetName} ${address.streetNumber}, ${
-            address.postalCode
-          } ${address.city}`}
-        </SimpleItem>
 
-        {isOpenNow && <SimpleItem icon={faDoorOpen}>Åben nu</SimpleItem>}
-
-        <div className="py-4 sm:py-8 leading-loose rich-text">
-          <ReactMarkdown source={description} />
-        </div>
-
-        {eventNodes.length > 0 && (
-          <Fragment>
-            <h2 className="mb-2 sm:mb-4 text-red-dark">
-              Kommende begivenheder
-            </h2>
+      <div className="p-4">
+        <Container>
+          <div className="sm:py-8 leading-loose">
             <Row>
-              {eventNodes.slice(0, 2).map(event => (
-                <Col xs="full" sm="1/2" key={event.slug}>
-                  <EventCard {...event} />
-                </Col>
-              ))}
+              <Col xs="1/1" sm="4/5">
+                <ReactMarkdown source={description} className="rich-text" />
+              </Col>
+
+              <Col xs="1/1" sm="1/5">
+                <h2 className="mb-2 text-xl text-red">Information</h2>
+
+                <div>
+                  <h3 className="my-2 text-base">Adresse</h3>
+                  <div className="my-2 text-sm">
+                    <div>{`${address.streetName} ${address.streetNumber}`}</div>
+                    <div className="text-grey-darker">
+                      {`${address.postalCode} ${address.city}`}
+                    </div>
+                  </div>
+
+                  <div>
+                    <a
+                      href={`https://www.google.com/maps/search/?api=1&query=${
+                        address.latitude
+                      },${address.longitude}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-sm no-underline text-red hover:underline"
+                    >
+                      Find rutevejledning
+                    </a>
+                  </div>
+                </div>
+
+                <div>
+                  <h3 className="my-2 text-base">Åbningstider</h3>
+                  {openingHours.map(openingHoursForDay => (
+                    <Row
+                      key={openingHoursForDay.dayOfWeek}
+                      className="-mx-4 mb-1"
+                    >
+                      <Col xs="1/3" sm="1/3">
+                        <div className="text-sm">
+                          {translateWeekDay(openingHoursForDay.dayOfWeek)}
+                        </div>
+                      </Col>
+                      <Col xs="2/3" sm="2/3">
+                        <div className="text-right text-sm text-grey-darker">
+                          {formatOpeningHours(
+                            openingHoursForDay.open,
+                            openingHoursForDay.close
+                          )}
+                        </div>
+                      </Col>
+                    </Row>
+                  ))}
+                </div>
+              </Col>
             </Row>
-          </Fragment>
-        )}
-        <h2 className="mt-8 mb-2 text-red-dark">Åbningstider</h2>
-        {openingHours.map(openingHoursForDay => (
-          <Row key={openingHoursForDay.dayOfWeek} className="-mx-4 mb-2">
-            <Col xs="1/2" sm="1/6">
-              {translateWeekDay(openingHoursForDay.dayOfWeek)}
-            </Col>
-            <Col xs="1/2" sm="1/6">
-              <div className="text-right">
-                {formatOpeningHours(
-                  openingHoursForDay.open,
-                  openingHoursForDay.close
-                )}
-              </div>
-            </Col>
-          </Row>
-        ))}
-        <h2 className="mt-8 mb-2 text-red-dark">Lokation</h2>
-        <div className="flex justify-between mb-4 text-sm sm:text-base">
-          <span>
-            {`${address.streetName} ${address.streetNumber}, ${
-              address.postalCode
-            } ${address.city}`}
-          </span>
+          </div>
 
-          <a
-            href={`https://www.google.com/maps/search/?api=1&query=${
-              address.latitude
-            },${address.longitude}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="ml-4 no-underline text-red-darker hover:underline"
-          >
-            Rutevejledning
-          </a>
-        </div>
-        <MapWithMarker
-          latitude={address.latitude}
-          longitude={address.longitude}
-          height={400}
-          width="100%"
-          className="rounded shadow-lg"
-        />
-
-        <h2 className="mt-8 mb-2 text-red-dark">Mere Information</h2>
-        <div className="mb-8">
-          {phoneNumber && (
-            <SimpleItem icon={faPhone}>
-              <a
-                href={`tel:${phoneNumber}`}
-                className="no-underline text-red-darker hover:underline"
-              >
-                {displayPhoneNumber(phoneNumber)}
-              </a>
-            </SimpleItem>
+          {eventNodes.length > 0 && (
+            <Fragment>
+              <h2 className="mb-2 sm:mb-4 text-red-dark">
+                Kommende begivenheder
+              </h2>
+              <Row>
+                {eventNodes.slice(0, 2).map(event => (
+                  <Col xs="full" sm="1/2" key={event.slug}>
+                    <EventCard {...event} />
+                  </Col>
+                ))}
+              </Row>
+            </Fragment>
           )}
-          {website && (
-            <SimpleItem icon={faBrowser}>
-              <a
-                href={website}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="no-underline text-red-darker hover:underline"
-              >
-                {displayURL(website)}
-              </a>
-            </SimpleItem>
-          )}
-        </div>
 
-        <h3 className="mt-8 mb-2 text-red-darker text-center">{`Arbejder du for ${name}?`}</h3>
-        <div className="mb-8 text-center">
-          <p className="mb-2">
-            Få adgang til profilen, administrer begivenheder, information og en
-            masse andre ting
-          </p>
-          <a href="#" className="no-underline text-red-darker hover:underline">
-            <Button type="button" kind="secondary">
-              {`Få adgang til ${name}`}
-            </Button>
-          </a>
-        </div>
-      </Container>
+          <h2 className="mt-8 mb-2 text-red-dark">Lokation</h2>
+          <div className="flex justify-between mb-4 text-sm sm:text-base">
+            <span>
+              {`${address.streetName} ${address.streetNumber}, ${
+                address.postalCode
+              } ${address.city}`}
+            </span>
+          </div>
+          <MapWithMarker
+            latitude={address.latitude}
+            longitude={address.longitude}
+            height={400}
+            width="100%"
+            className="rounded shadow-lg"
+          />
+
+          <h2 className="mt-8 mb-2 text-red-dark">Mere Information</h2>
+          <div className="mb-8">
+            {phoneNumber && (
+              <SimpleItem icon={faPhone}>
+                <a
+                  href={`tel:${phoneNumber}`}
+                  className="no-underline text-red-darker hover:underline"
+                >
+                  {displayPhoneNumber(phoneNumber)}
+                </a>
+              </SimpleItem>
+            )}
+            {website && (
+              <SimpleItem icon={faBrowser}>
+                <a
+                  href={website}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="no-underline text-red-darker hover:underline"
+                >
+                  {displayURL(website)}
+                </a>
+              </SimpleItem>
+            )}
+          </div>
+
+          <h3 className="mt-8 mb-2 text-red-darker text-center">{`Arbejder du for ${name}?`}</h3>
+          <div className="mb-8 text-center">
+            <p className="mb-2">
+              Få adgang til profilen, administrer begivenheder, information og
+              en masse andre ting
+            </p>
+            <a
+              href="#"
+              className="no-underline text-red-darker hover:underline"
+            >
+              <Button type="button" kind="secondary">
+                {`Få adgang til ${name}`}
+              </Button>
+            </a>
+          </div>
+        </Container>
+      </div>
       <script type="application/ld+json">
         {businessStructuredData(business)}
       </script>
